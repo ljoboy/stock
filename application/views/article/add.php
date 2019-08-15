c7fdf616243ca498c5bb92f74446c75a25490cfe
Jonathan Yombo <jonathanyombo@gmail.com>
2019-08-15 21:03:24 +0200

    [UPDATE] add article's CRUD
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

---
+++ application/views/article/add.php
@@ -0,0 +1,55 @@
+<div class="row">
+    <div class="col-md-12">
+      	<div class="box box-info">
+            <div class="box-header with-border">
+              	<h3 class="box-title">Article > Ajouter</h3>
+            </div>
+            <?php echo form_open('article/add'); ?>
+          	<div class="box-body">
+          		<div class="row clearfix">
+					<div class="col-md-6">
+						<label for="code" class="control-label"><span class="text-danger">*</span>Code</label>
+						<div class="form-group">
+							<input type="text" name="code" value="<?php echo $this->input->post('code'); ?>" class="form-control" id="code" />
+							<span class="text-danger"><?php echo form_error('code');?></span>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="fournisseur" class="control-label"><span class="text-danger">*</span>Fournisseur</label>
+						<div class="form-group">
+							<input type="text" name="fournisseur" value="<?php echo $this->input->post('fournisseur'); ?>" class="form-control" id="fournisseur" />
+							<span class="text-danger"><?php echo form_error('fournisseur');?></span>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="qte" class="control-label"><span class="text-danger">*</span>Qte</label>
+						<div class="form-group">
+							<input type="text" name="qte" value="<?php echo $this->input->post('qte'); ?>" class="form-control" id="qte" />
+							<span class="text-danger"><?php echo form_error('qte');?></span>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="qte_min" class="control-label"><span class="text-danger">*</span>Qte Min</label>
+						<div class="form-group">
+							<input type="text" name="qte_min" value="<?php echo $this->input->post('qte_min'); ?>" class="form-control" id="qte_min" />
+							<span class="text-danger"><?php echo form_error('qte_min');?></span>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="date_creation" class="control-label"><span class="text-danger">*</span>Date Creation</label>
+						<div class="form-group">
+							<input type="text" name="date_creation" value="<?php echo $this->input->post('date_creation'); ?>" class="form-control" id="date_creation" />
+							<span class="text-danger"><?php echo form_error('date_creation');?></span>
+						</div>
+					</div>
+				</div>
+			</div>
+          	<div class="box-footer">
+            	<button type="submit" class="btn btn-success">
+            		<i class="fa fa-check"></i> Enregister
+            	</button>
+          	</div>
+            <?php echo form_close(); ?>
+      	</div>
+    </div>
+</div>
---
+++ application/views/article/index.php
@@ -0,0 +1,40 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Articles > Lister</h3>
+            	<div class="box-tools">
+                    <a href="<?php echo site_url('article/add'); ?>" class="btn btn-success btn-sm"> Ajouter</a>
+                </div>
+            </div>
+            <div class="box-body">
+                <table class="table table-striped">
+                    <tr>
+						<th>Id Article</th>
+						<th>Code</th>
+						<th>Fournisseur</th>
+						<th>Qte</th>
+						<th>Qte Min</th>
+						<th>Date Creation</th>
+						<th>Actions</th>
+                    </tr>
+                    <?php foreach($articles as $a){ ?>
+                    <tr>
+						<td><?php echo $a['id_article']; ?></td>
+						<td><?php echo $a['code']; ?></td>
+						<td><?php echo $a['fournisseur']; ?></td>
+						<td><?php echo $a['qte']; ?></td>
+						<td><?php echo $a['qte_min']; ?></td>
+						<td><?php echo $a['date_creation']; ?></td>
+						<td>
+                            <a href="<?php echo site_url('article/edit/'.$a['id_article']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> > Modifier</a>
+                            <a href="<?php echo site_url('article/remove/'.$a['id_article']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Effacer</a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </table>
+                                
+            </div>
+        </div>
+    </div>
+</div>
